0e39df61900e34af706893c12433f6de67014708
rcchcz <rcchcz@gmail.com>
2022-06-25 11:17:57 -0300

    first steps learning python :)

---
+++ python-studies/palindromeNumber.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        
+        aux = x
+        b = 0
+        while aux:
+            b = b * 10 + aux % 10
+            aux //= 10
+            
+        return x == b
---
+++ python-studies/twoSum.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        lib = {}
+        for i, n in enumerate(nums):
+            if n in lib:
+                return [lib[n], i]
+            lib[target - n] = i
+        return []
