1a38891dc47c54b0b8556f06312ef6fa061f19dc
Rita Cruz <rcchcz@gmail.com>
2022-06-28 08:02:41 -0300

    Update longestCommonPrefix.py

--- python-studies/longestCommonPrefix.py
+++ python-studies/longestCommonPrefix.py
@@ -1,4 +1,4 @@
-#https://leetcode.com/problems/longest-common-prefix/
+# https://leetcode.com/problems/longest-common-prefix/
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         min_len = len(min(strs, key=len))
