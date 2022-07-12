6dc255c79fe294fb9dd4032a83b50978dc2b0cfb
Rita Cruz <rcchcz@gmail.com>
2022-07-12 17:34:38 -0300

    Update dsatur.cpp

--- dsatur.cpp
+++ dsatur.cpp
@@ -13,20 +13,19 @@ void add_edge(vector<int> graph[], int a, int b) {
 // information related to vertices
 class vertexInfo {
   public:
-    // Saturation degree of the vertex.
+    // saturation degree
     int sat;
   
     // degree in uncolored graph
     int deg;
   
-    // index of vertex.
+    // index
     int vertex;
 };
  
 struct maxSat {
-  bool operator()(const vertexInfo &lhs,
-                  const vertexInfo &rhs) const {
-      return tie(lhs.sat, lhs.deg, lhs.vertex) > tie(rhs.sat, rhs.deg, rhs.vertex); }
+  bool operator()(const vertexInfo &lhs, const vertexInfo &rhs) const {
+    return tie(lhs.sat, lhs.deg, lhs.vertex) > tie(rhs.sat, rhs.deg, rhs.vertex); }
 };
 
 // print k independent sets 
@@ -48,74 +47,69 @@ void printksets(vector<int> &color, set<int> &ans) {
 // print color of each vertex of graph
 void printcolor(vector<int> &color) {
   for (int i = 0; i < color.size(); i++) {
-      cout << "The Color of the vertex " << i << " is " << color[i] << endl;
+      cout << "The color of the vertex " << i << " is " << color[i] << endl;
   }
 }
  
 // DSatur algorithm
 int DSatur(vector<int> graph[], int V) {
-    int u, i;
-    // Create vector to store status of used colors.
-    vector<bool> use(V, false);
+  int u, i;
+  // create vector to store status of used colors
+  vector<bool> use(V, false);
 
-    // Create vector to store colors.
-    vector<int> color(V), d(V);
-    vector<set<int>> adjCols(V);
-    set<vertexInfo, maxSat> Q;
-    set<vertexInfo, maxSat>::iterator maxPtr;
+  // create vector to store colors.
+  vector<int> color(V), d(V);
+  vector<set<int>> adjCols(V);
+  set<vertexInfo, maxSat> Q;
+  set<vertexInfo, maxSat>::iterator maxPtr;
 
-    for(u = 0; u < V; u++) {
-        color[u] = -1;
-        d[u] = graph[u].size();
-        adjCols[u] = set<int>();
-        Q.emplace(vertexInfo{0, d[u], u});
+  for(u = 0; u < V; u++) {
+    color[u] = -1;
+    d[u] = graph[u].size();
+    adjCols[u] = set<int>();
+    Q.emplace(vertexInfo{0, d[u], u});
+  }
+
+  while (!Q.empty()) {
+    maxPtr = Q.begin();
+    u = (*maxPtr).vertex;
+    Q.erase(maxPtr);
+
+    for(int v : graph[u]) {
+      if(color[v] != -1) { use[color[v]] = true; }
     }
- 
-    while (!Q.empty()) {
-        maxPtr = Q.begin();
-        u = (*maxPtr).vertex;
-        Q.erase(maxPtr);
- 
-        for(int v : graph[u]) {
-          if(color[v] != -1) { use[color[v]] = true; }
-        }
-              
-        i = 0;
-        while(i != use.size()) {
-            if (use[i] == false) { break; }
-            i++;
-        }
-      
-        for(auto v : graph[u]) {
-          if (color[v] != -1) { use[color[v]] = false; }
-        }
           
-        color[u] = i;
-        for(auto v : graph[u]) {
-            if(color[v] == -1) {
-                Q.erase(
-                    {int(adjCols[v].size()),
-                     d[v], v});
-                adjCols[v].insert(i);
-                d[v]--;
-                Q.emplace(vertexInfo{
-                    int(adjCols[v].size()),
-                    d[v], v});
-            }
-        }
+    i = 0;
+    while(i != use.size()) {
+      if (use[i] == false) { break; }
+      i++;
     }
   
-    set<int> ans;
-    for(int i = 0; i < V; i++) { ans.insert(color[i]); }
-        
-    printksets(color, ans);
-    // Return Chromatic number.
-    // Print the Chromatic number of graph coloring.
-    cout << "The Chromatic number is : ";
-    return ans.size();
+    for(auto v : graph[u]) {
+      if (color[v] != -1) { use[color[v]] = false; }
+    }
+      
+    color[u] = i;
+    for(auto v : graph[u]) {
+      if(color[v] == -1) {
+        Q.erase({int(adjCols[v].size()), d[v], v});
+        adjCols[v].insert(i);
+        d[v]--;
+        Q.emplace(vertexInfo{int(adjCols[v].size()),d[v], v});
+      }
+    }
+  }
+
+  set<int> ans;
+  for(int i = 0; i < V; i++) { ans.insert(color[i]); }
+      
+  printksets(color, ans);
+  
+  // return chromatic number
+  return ans.size();
 }
  
-// Function to calculate Chromatic numvber.
+// calculate the chromatic number
 int chromatic_number() { 
   // 'v' vertices, 'e' edges
   int v, e;
@@ -134,6 +128,7 @@ int chromatic_number() {
 }
 
 int main() {
-    cout << chromatic_number();
-    return 0;
+  int cn = chromatic_number();
+  cout << "Total containers: " << cn << endl;
+  return 0;
 }
