3f3d7890c256d11e6e26b6139923f9e1d35f6bb1
Rita Cruz <rcchcz@gmail.com>
2023-01-10 09:24:08 -0300

    add problem #100

---
+++ python-studies/same_tree.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/same-tree/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def dfs(p, q):
+            if not p and not q:
+                return True
+            elif (p and not q) or (q and not p) or p.val != q.val:
+                return False
+            
+            return dfs(p.left, q.left) and dfs(p.right, q.right)
+        
+        return dfs(p, q)
