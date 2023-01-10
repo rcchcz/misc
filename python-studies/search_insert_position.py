ababbebff90ef731fbb79eab6b9e59985e06a1aa
Rita Cruz <rcchcz@gmail.com>
2023-01-10 16:32:32 -0300

    add problem #35

---
+++ python-studies/search_insert_position.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/search-insert-position/
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        nums.append(float('inf'))
+        l, r = 0, len(nums)-1
+
+        while l < r:
+            mid = (l + r) // 2
+            if target <= nums[mid]:
+                r = mid
+            else:
+                l = mid + 1
+        
+        return l
