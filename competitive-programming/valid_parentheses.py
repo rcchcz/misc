fa55b8c6a64aca6f5af7379b9703b56fdd7b6dcd
Rita Cruz <rcchcz@gmail.com>
2023-04-10 09:46:52 -0300

    add problem #20

--- competitive-programming/valid_parentheses.py
+++ competitive-programming/valid_parentheses.py
@@ -2,15 +2,11 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        closeToOpen = { ")" : "(", "]" : "[", "}" : "{" }
+        lookup = {")": "(", "}":"{", "]":"["}
 
-        for c in s:
-            if c in closeToOpen:
-                if stack and stack[-1] == closeToOpen[c]:
-                    stack.pop()
-                else:
-                    return False
-            else:
-                stack.append(c)
-
-        return True if not stack else False
+        for p in s:
+            if p in lookup.values(): stack.append(p)
+            elif stack and lookup[p] == stack[-1]: stack.pop()
+            else: return False
+        
+        return stack == []
